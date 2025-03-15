9fdd6f65e11c7324f0c8485c387eccc1dd19b1c2
BİLAL ERKAN TAŞKIN <148206650+30blltskn30@users.noreply.github.com>
2025-03-15 17:22:40 +0300

    Add files via upload

---
+++ CarRace/CarRace/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿using System.Drawing;
+
+namespace CarRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.btn_restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(1062, 312);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(1062, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.ErrorImage = null;
+            this.pictureBox3.ImageLocation = "";
+            this.pictureBox3.Location = new System.Drawing.Point(1062, 208);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 77);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "salam\r\n";
+            this.notifyIcon1.Visible = true;
+            // 
+            // btnstart
+            // 
+            this.btnstart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnstart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnstart.Location = new System.Drawing.Point(87, 12);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(146, 42);
+            this.btnstart.TabIndex = 3;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = false;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.BackColor = System.Drawing.Color.Red;
+            this.btnstop.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnstop.Location = new System.Drawing.Point(263, 12);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(146, 42);
+            this.btnstop.TabIndex = 4;
+            this.btnstop.Text = "Stop";
+            this.btnstop.UseVisualStyleBackColor = false;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_restart.Location = new System.Drawing.Point(441, 12);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(146, 42);
+            this.btn_restart.TabIndex = 5;
+            this.btn_restart.Text = "Reset";
+            this.btn_restart.UseVisualStyleBackColor = false;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1303, 506);
+            this.Controls.Add(this.btn_restart);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.Button btn_restart;
+    }
+}
+
---
+++ CarRace/CarRace/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRace
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int pictureBox1Hiz, pictureBox2Hiz, pictureBo3xHiz;
+        public Form1()
+
+
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+
+
+        }
+
+
+        // Random rnd = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 50;
+            BaslangicAyarlari();
+            timer1.Start();
+        }
+
+
+        private void BaslangicAyarlari()
+        {
+
+            pictureBox1Hiz = rnd.Next(5, 15);
+            pictureBox2Hiz = rnd.Next(5, 15);
+            pictureBo3xHiz = rnd.Next(5, 15);
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            BaslangicAyarlari();
+            timer1.Start();
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            BaslangicAyarlari();
+            pictureBox1.Left = 800;
+            pictureBox2.Left = 800;
+            pictureBox3.Left = 800;
+
+
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= pictureBox1Hiz;
+            pictureBox2.Left -= pictureBox2Hiz;
+            pictureBox3.Left -= pictureBo3xHiz;
+
+
+            if (pictureBox1.Left <= 30 ||
+           pictureBox2.Left <= 30 ||
+           pictureBox3.Left <= 30)
+            {
+                timer1.Stop(); // Yarışı durdur
+
+                if (pictureBox1.Left <= 30)
+                    MessageBox.Show("Winner is Konsol Oyun Gurulu Bakış"); // Kazananı belirlemek için ek kod ekleyebilirsin
+                else if (pictureBox2.Left <= 30)
+                    MessageBox.Show("Winner is IshowSpeed");
+                else if (pictureBox1.Left <= 30)
+                    MessageBox.Show("Winner is Mcqueen");
+            }
+        }
+
+
+
+    }
+}
